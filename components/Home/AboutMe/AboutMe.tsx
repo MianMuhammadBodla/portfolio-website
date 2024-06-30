3891ac217174db39fb912ce1d0e3885503600fb0
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2024-06-30 18:42:10 +0500

    update

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -3,8 +3,8 @@ import Img from "../../../components/smallComp/image/Img";
 import ArrowIcon from "../../../components/Icons/ArrowIcon";
 export default function AboutMe(props) {
   const technologies = [
-    ["Next.js",],
-    ["Python"],
+    ["Next.js","React","Tailwind CSS","TypeScript"],
+    ["Docker","Cloud Computing","Python","Generative AI"],
   ];
   return (
     <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col  items-center py-20 bg-AAprimary">
